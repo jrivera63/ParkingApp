d9299b07cfc9cb6a6122cc99ac944611f3c40dca
josiah <jrivera63@students.mchenry.edu>
2022-10-31 21:27:23 -0500

    Correct Version of Assignment

--- app/src/main/java/com/example/parkingapp/MainActivity.kt
+++ app/src/main/java/com/example/parkingapp/MainActivity.kt
@@ -9,25 +9,36 @@ import android.widget.TextView
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
-    val costPerHour = 1.50
-    var numberOfHours: Int = 0
-    var totalCharges: Double = 0.0
-    var lotChoice = ""
+    var meal = 0.0
+    var tipAmount: Double = 0.0
+    var tipChoice = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val hours: EditText = findViewById(R.id.idHours)
+
+        val mealPrice: EditText = findViewById(R.id.idPrice)
         val group: Spinner = findViewById(R.id.spnGroup)
         val charges: Button = findViewById(R.id.idBtnCharge)
         val result: TextView = findViewById(R.id.idResult)
 
         charges.setOnClickListener(){
-            numberOfHours = hours.text.toString().toInt()
+            tipChoice = group.selectedItem.toString().toInt()
+            meal = mealPrice.text.toString().toDouble()
+            if (tipChoice == 5){
+                tipAmount = meal * .05
+            } else if (tipChoice == 10){
+                tipAmount = meal * .10
+            } else if (tipChoice == 15){
+                tipAmount = meal * .15
+            } else if (tipChoice == 20){
+                tipAmount = meal * .20
+            } else if (tipChoice == 25){
+                tipAmount = meal * .25
+            }
+
             val currency = DecimalFormat("$###,###.00")
-            totalCharges = costPerHour * numberOfHours.toDouble()
-            lotChoice = group.selectedItem.toString()
-            val totalChargesFormat = currency.format(totalCharges)
-            result.text = "Cost for parking in $lotChoice for $numberOfHours hours is $totalChargesFormat"
+            val totalChargesFormat = currency.format(tipAmount)
+            result.text = "A $tipChoice% tip for a $$meal meal is $totalChargesFormat"
         }
     }
 }
